50ebe3df7300d3f254fbb299638e403b377ee082
Jorge Escobar <jorge@fromzero.io>
2019-09-06 09:09:22 -0400

    First commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DATABASE_NAME = os.environ["DATABASE_NAME"]
+MYSQL_ROOT_PASSWORD = os.environ["MYSQL_ROOT_PASSWORD"]
