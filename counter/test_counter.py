f8baab15e6f1d9c9a1e12c7ed91516fba82a8ffa
Jorge Escobar <jorge@fromzero.io>
2019-10-04 09:07:47 -0400

    completed step-6

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,39 +7,14 @@ from counter.models import counter_table, metadata as CounterMetadata
 # Create this tests' models, needs a module scope
 # to inherit from the model-level connection
 @pytest.fixture(scope="module")
-def create_all(create_db):
+def create_counter_tables(create_db):
     engine = create_engine(create_db["DB_URI"] + "/" + create_db["DATABASE_NAME"])
     CounterMetadata.bind = engine
     CounterMetadata.create_all()
 
 
 @pytest.mark.asyncio
-async def test_initial_response(create_test_client, create_all):
+async def test_initial_response(create_test_client, create_counter_tables):
     response = await create_test_client.get("/")
     body = await response.get_data()
     assert "Counter: 1" in str(body)
-
-
-@pytest.mark.asyncio
-async def test_second_response(create_test_app, create_test_client, create_all):
-    # Reset the tables
-    CounterMetadata.drop_all()
-    CounterMetadata.create_all()
-
-    # first hit
-    response = await create_test_client.get("/")
-    body = await response.get_data()
-
-    # second hit
-    response = await create_test_client.get("/")
-    body = await response.get_data()
-    assert "Counter: 2" in str(body)
-
-    # check on the model itself
-    async with create_test_app.app_context():
-        conn = current_app.sac
-        counter_query = select([counter_table.c.count])
-        result = await conn.execute(counter_query)
-        result_row = await result.first()
-        count = result_row[counter_table.c.count]
-        assert count == 2
